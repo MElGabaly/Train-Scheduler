12fea5f1659b094e68d1299f502cec998007217d
gabaly <Gabaly@mohameds-mbp-2.attlocal.net>
2019-04-30 00:16:06 -0700

    Modifications

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+ 
+ $(document).ready(function () {
+
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyAtHMH6kP3bZub_Q0S_mvlPkGR0S7qZx6M",
+    authDomain: "trainhomework-9b24f.firebaseapp.com",
+    databaseURL: "https://trainhomework-9b24f.firebaseio.com",
+    projectId: "trainhomework-9b24f",
+    storageBucket: "trainhomework-9b24f.appspot.com",
+    messagingSenderId: "631356520018"
+  };
+  firebase.initializeApp(config);
+
+
+     
+ });
+ 
+ 
